8fadab96ed7428ad2208e86b4dc629cdd441671b
lbrayner <lbrayner@users.noreply.github.com>
2020-08-01 13:07:41 -0300

    Added 9 extra workspaces (F1 to F9)

--- config.h
+++ config.h
@@ -22,7 +22,8 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9",
+                                 "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -122,6 +123,15 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
+	TAGKEYS(                        XK_F1,                     9)
+	TAGKEYS(                        XK_F2,                     10)
+	TAGKEYS(                        XK_F3,                     11)
+	TAGKEYS(                        XK_F4,                     12)
+	TAGKEYS(                        XK_F5,                     13)
+	TAGKEYS(                        XK_F6,                     14)
+	TAGKEYS(                        XK_F7,                     15)
+	TAGKEYS(                        XK_F8,                     16)
+	TAGKEYS(                        XK_F9,                     17)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
